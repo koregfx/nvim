40aa195b2ab0ce8c3215f4748431acb96b11f77e
koregfx <koregfx@gmail.com>
2023-12-30 08:30:22 +0100

    fix for lsp-config file bad format

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -99,21 +99,11 @@ return {
                                 runtime = {
                                     version = "LuaJIT",
                                 },
-                                    -- Tell the language server which version of Lua you're using
-                                    -- (most likely LuaJIT in the case of Neovim)
-                                    version = "LuaJIT",
-                                },
-                                -- Make the server aware of Neovim runtime files
                                 workspace = {
                                     checkThirdParty = false,
                                     library = {
                                         vim.env.VIMRUNTIME,
                                     },
-                                        -- "${3rd}/luv/library"
-                                        -- "${3rd}/busted/library",
-                                    },
-                                    -- or pull in all of 'runtimepath'. NOTE: this is a lot slower
-                                    -- library = vim.api.nvim_get_runtime_file("", true)
                                 },
                             },
                         })
