a329736de076e59444de398384f2480dcabdc199
koregfx <koregfx@gmail.com>
2024-01-03 11:50:06 +0100

    customize lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -22,24 +22,6 @@ local mode_color = {
     ["!"] = mocha.red,
     t = mocha.red,
 }
-local function split(pString, pPattern)
-    local Table = {} -- NOTE: use {n = 0} in Lua-5.0
-    local fpat = "(.-)" .. pPattern
-    local last_end = 1
-    local s, e, cap = pString:find(fpat, 1)
-    while s do
-        if s ~= 1 or cap ~= "" then
-            table.insert(Table, cap)
-        end
-        last_end = e + 1
-        s, e, cap = pString:find(fpat, last_end)
-    end
-    if last_end <= #pString then
-        cap = pString:sub(last_end)
-        table.insert(Table, cap)
-    end
-    return Table
-end
 return {
     "nvim-lualine/lualine.nvim",
     dependencies = { "nvim-tree/nvim-web-devicons" },
@@ -103,92 +85,20 @@ return {
 
             lualine_x = {
                 { "fileformat" },
-                { "diagnostics" },
-            },
-
-            lualine_y = {
                 {
-                    function()
-                        return " "
-                    end,
-                    color = function()
-                        return { fg = mocha.crust, bg = mocha.green, gui = "bold" }
-                    end,
-                    padding = { right = 0, left = 0 },
-                    separator = { left = "" },
+                    "diagnostics",
+                    sections = { "error" },
+                    diagnostics_color = { error = { fg = mocha.white } },
                 },
                 {
-                    function()
-                        local msg = "No Active Lsp"
-                        local buf_ft = vim.api.nvim_buf_get_option(0, "filetype")
-                        local clients = vim.lsp.get_active_clients()
-                        if next(clients) == nil then
-                            return msg
-                        end
-                        for _, client in ipairs(clients) do
-                            local filetypes = client.config.filetypes
-                            if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 then
-                                return client.name
-                            end
-                        end
-                        return msg
-                    end,
-                    separator = { right = "" },
-                    color = { fg = mocha.green, gui = "bold" },
-                },
-                {
-                    function()
-                        return "　"
-                    end,
-                    padding = 0,
-                    separator = {},
-                    color = { bg = mocha.mantle },
+                    "diagnostics",
+                    sections = { "warn" },
+                    diagnostics_color = { warn = { fg = mocha.white } },
                 },
             },
+            lualine_y = { "filetype", "progress" },
             lualine_z = {
-                {
-                    function()
-                        return " "
-                    end,
-                    color = function()
-                        return { fg = mocha.crust, bg = mocha.red, gui = "bold" }
-                    end,
-                    padding = { right = 0, left = 0 },
-                    separator = { left = "" },
-                },
-                {
-                    function()
-                        local path = vim.loop.cwd()
-                        local path_table = split(path, "\\")
-                        return path_table[#path_table]
-                    end,
-                    path = 2,
-                    mode = 2,
-                    separator = { right = "" },
-                    color = { fg = mocha.red, bg = mocha.surface0, gui = "bold" },
-                },
-                {
-                    function()
-                        return "　"
-                    end,
-                    padding = 0,
-                    separator = {},
-                    color = { bg = mocha.mantle },
-                },
-
-                {
-                    function()
-                        return " "
-                    end,
-                    color = { fg = mocha.crust, bg = mocha.peach, gui = "bold" },
-                    padding = { right = 0, left = 0 },
-                    separator = { left = "" },
-                },
-                {
-                    "location",
-                    separator = { right = "" },
-                    color = { fg = mocha.peach, bg = mocha.surface0 },
-                },
+                { "location", separator = { right = "" } },
             },
         },
         inactive_sections = {
