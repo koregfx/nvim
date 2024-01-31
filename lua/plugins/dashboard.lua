41a2d7d9e2f23578e98b5895cd5b388960c4162d
koregfx <koregfx@gmail.com>
2024-01-31 08:01:06 +0100

    change dashboard logo

--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -3,12 +3,13 @@ local M = {
 	event = "VimEnter",
 	opts = function()
 		local logo = [[
-         ██╗      █████╗ ███████╗██╗   ██╗██╗   ██╗██╗███╗   ███╗          Z
-         ██║     ██╔══██╗╚══███╔╝╚██╗ ██╔╝██║   ██║██║████╗ ████║      Z    
-         ██║     ███████║  ███╔╝  ╚████╔╝ ██║   ██║██║██╔████╔██║   z       
-         ██║     ██╔══██║ ███╔╝    ╚██╔╝  ╚██╗ ██╔╝██║██║╚██╔╝██║ z         
-         ███████╗██║  ██║███████╗   ██║    ╚████╔╝ ██║██║ ╚═╝ ██║           
-         ╚══════╝╚═╝  ╚═╝╚══════╝   ╚═╝     ╚═══╝  ╚═╝╚═╝     ╚═╝           
+             ████ ██████           █████      ██                     
+            ███████████             █████                             
+            █████████ ███████████████████ ███   ███████████   
+           █████████  ███    █████████████ █████ ██████████████   
+          █████████ ██████████ █████████ █████ █████ ████ █████   
+        ███████████ ███    ███ █████████ █████ █████ ████ █████  
+       ██████  █████████████████████ ████ █████ █████ ████ ██████ 
     ]]
 
 		logo = string.rep("\n", 8) .. logo .. "\n\n"
@@ -28,9 +29,6 @@ local M = {
           { action = "ene | startinsert",                                        desc = " New file",        icon = " ", key = "n" },
           { action = "Telescope oldfiles",                                       desc = " Recent files",    icon = " ", key = "r" },
           { action = "Telescope live_grep",                                      desc = " Find text",       icon = " ", key = "g" },
-          { action = [[lua require("lazyvim.util").telescope.config_files()()]], desc = " Config",          icon = " ", key = "c" },
-          { action = 'lua require("persistence").load()',                        desc = " Restore Session", icon = " ", key = "s" },
-          { action = "LazyExtras",                                               desc = " Lazy Extras",     icon = " ", key = "x" },
           { action = "Lazy",                                                     desc = " Lazy",            icon = "󰒲 ", key = "l" },
           { action = "qa",                                                       desc = " Quit",            icon = " ", key = "q" },
         },
