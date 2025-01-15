f0b24f693deeb9df31147c380bfda5824c0dd205
koregfx <koregfx@gmail.com>
2025-01-15 13:44:51 +0100

    ts better plugins

---
+++ lua/plugins/ts-autotag.lua
@@ -0,0 +1,10 @@
+local M = {
+  'windwp/nvim-ts-autotag',
+  opts = {
+    enable_close = true, -- Auto close tags
+    enable_rename = true, -- Auto rename pairs of tags
+    enable_close_on_slash = false, -- Auto close on trailing </
+  },
+}
+
+return M
